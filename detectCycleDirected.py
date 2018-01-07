90793a0be961a74db03272a1d47552e1a65db3a0
udwivedi394 <utkarshdwivedi394@gmail.com>
2018-01-07 10:32:41 +0530

    Implement detectCycle to find if there are any cycles in the graph

--- detectCycleDirected.py
+++ detectCycleDirected.py
@@ -1,44 +1,57 @@
 import directedGraph as dirG
-import time
 
+#Detects whether any cycle is present in Directed Graph,
+#True->cycle present, False->cycle absent
 def detectCycle(graph):
 	vertices = graph.getVertices()
-	print vertices
-	recursionStack = []
+	
+	#Keep track of visited vetices
+	visited = [False]*len(vertices)
+	#Keep track of the vertices in the running path, to check if any vertex falls in the running stack
+	recursionStack = [False]*len(vertices)
+
+	#To check every separate tree in the graph
 	for i in range(len(vertices)):
-		visited = [False]*len(vertices)
-		if detectCycleUtil(vertices,i,visited,recursionStack):
-			return True
-		print
+		if visited[i]==False:
+			if detectCycleUtil(vertices,i,visited,recursionStack):
+				return True
 	return False
 
+#Utility to perform above operations
 def detectCycleUtil(graph,v,visited,recursionStack):
+	#Mark the current vertex as visited in Visited Loopkup
 	visited[v] = True
+	#Mark the current vertex as visited in current path
+	recursionStack[v] = True
 	print v,
 	
-	recursionStack.append(v)
-	
 	check=False
+	#Iterate all the edges in the adjacency of current vertex 
 	for i in graph[v]:
+		#If the vertex is not already visited
 		if visited[i]==False:
 			check = detectCycleUtil(graph,i,visited,recursionStack)
-
-		if i in recursionStack:
+		
+		#If not visited, check if the current vertex falls in the current path
+		elif recursionStack[i]==True:
 			return True
 
 		if check:
 			return True
-	recursionStack.pop()
+
+	#BackTrack, unmark the current vertex as visited in recursion stack
+	recursionStack[v]=False
 	return False
 
 g1 = dirG.DirectedGraph()
 g1.addEdge(0,1)
 g1.addEdge(0,2)
-g1.addEdge(1,1)
+#g1.addEdge(1,1)
 g1.addEdge(1,2)
-g1.addEdge(2,0)
+#g1.addEdge(2,0)
 g1.addEdge(2,3)
 g1.addEdge(3,4)
+g1.addEdge(5,5)
 
 g1.printGraph()
-print detectCycle(g1)
+print "\n",detectCycle(g1)
